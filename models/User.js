9a94e949c5f6017c53147df6d098dedc7aacff4c
Naman Verma <81072434+naman-verma-01@users.noreply.github.com>
2022-03-16 15:33:47 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../services/otp_model_connection");
+
+const User = sequelize.define('userm', {
+    // Model attributes are defined here
+    
+    username: {
+        type: Sequelize.STRING
+        // allowNull defaults to true
+    },
+    password: {
+        type: Sequelize.STRING
+        // allowNull defaults to true
+    },
+    emailid: {
+        type: Sequelize.STRING
+        // allowNull defaults to true
+    },
+    MobileNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+    
+}, {
+    // Other model options go here
+});
+
+
+module.exports = User;
