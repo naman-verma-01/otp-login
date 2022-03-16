8c1cd28fdf05693187a8b11e282faf437253351f
Naman Verma <81072434+naman-verma-01@users.noreply.github.com>
2022-03-16 15:50:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const otp = require("./routes/otpVerify")
 const apiDocs = require("./routes/apiDocs")
 const otplogin = require("./routes/otpLogin")
 const User = require("./models/User")
+const signup = require("./routes/userSignup")
+
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,7 +18,7 @@ app.use(bodyParser.json());
 app.use("/", apiDocs)
 app.use("/otp", otp)
 app.use("/otplogin",otplogin)
-
+app.use("/signup", signup)
 const port = 8000;
 
 app.listen(process.env.PORT || port, () => console.log("server started on port " + port));
