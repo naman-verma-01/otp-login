824ebed17c7d3adf32bc492b6705b45aec53281e
Naman Verma <81072434+naman-verma-01@users.noreply.github.com>
2022-03-16 15:33:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 require('dotenv').config()
 const express = require("express");
 const Otp = require("./models/otp");
-const sequelize = require("./services/Connection");
+const sequelize = require("./services/otp_model_connection");
 const bodyParser = require('body-parser');
 const app = express();
 const jwt = require("jsonwebtoken");
 const otp = require("./routes/otpVerify")
 const apiDocs = require("./routes/apiDocs")
+const otplogin = require("./routes/otpLogin")
+const User = require("./models/User")
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("/", apiDocs)
 app.use("/otp", otp)
+app.use("/otplogin",otplogin)
 
 const port = 8000;
 
