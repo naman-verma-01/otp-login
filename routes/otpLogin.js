24ceb939351f31fa01ecd0ccc50a016b8222fc2b
Naman Verma <81072434+naman-verma-01@users.noreply.github.com>
2022-03-16 16:03:12 +0530

    Update otpLogin.js

--- routes/otpLogin.js
+++ routes/otpLogin.js
@@ -3,10 +3,14 @@ const router = express.Router()
 const Otp = require("../models/otp");
 const User = require("../models/User");
 const person = require("../services/otploginUser")
+const jwt = require("jsonwebtoken");
+require('dotenv').config()
 
 router.get("/",person ,async (req, res) => {
-
-    res.json({ "Person": req.person[0] })
+    jwt_user = {user: req.person[0].username,pass: req.person[0].password};
+        
+    const accesstoken = jwt.sign(jwt_user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ "Person": req.person[0],"Token": accesstoken })
 })
 
 
